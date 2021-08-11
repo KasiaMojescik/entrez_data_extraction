9f4b0e9564f9501674f3380589342fd7b562adf6
Kasia Mojescik <katarzynamojescik@gmail.com>
2021-08-11 17:27:34 +0100

    Extracting data using batches of ids

--- entrezService.py
+++ entrezService.py
@@ -1,4 +1,4 @@
-from Bio import Entrez
+from Bio import Entrez, Medline
 
 
 
@@ -8,25 +8,39 @@ class EntrezSearchService:
     def __init__(self, db):
         self.db = db
 
-    def search_medline(self, retstart, retmax, totalRecords, query, email):
+    def append_data(self, search, retstart, retmax, final):
+        handle = Entrez.read(search)
+        web_env = handle['WebEnv']
+        retstart += retmax
+        try:
+            final.append(handle['IdList'])
+        except Exception as e:
+            raise IOError(str(e))
+        finally:
+            search.close()
+        return final, web_env, retstart
+
+    def perform_esearch(self, retstart, retmax, totalRecords, query, email):
         Entrez.email = email
         final = []
-        while (retstart < totalRecords):
-            search = Entrez.esearch(db=self.db, retstart=retstart, retmax=retmax, term=query, usehistory='y')
-            handle = Entrez.read(search)
-            retstart += retmax
-            try:
-                final += handle['IdList']
-            except Exception as e:
-                raise IOError(str(e))
-            finally:
-                search.close()
-        print("Finished searching " + self.db + " for all ids. Number of ids retrieved: ", len(final))
+        search = Entrez.esearch(db=self.db, retstart=retstart, retmax=retmax, term=query, usehistory='y')
+        first_search = self.append_data(search, retstart, retmax, final)
+        final = first_search[0]
+        web_env = first_search[1]
+        retstart = first_search[2]
+        while retstart < totalRecords:
+            search = Entrez.esearch(db=self.db, retstart=retstart, retmax=retmax, term=query, web_env=web_env,
+                                    usehistory='y')
+            data = self.append_data(search, retstart, retmax, final)
+            final = data[0]
+            retstart = data[2]
+
+        print("Finished searching. Roughly the number of ids extracted : ", len(final)*len(final[0]))
         return final
 
     def fetch_rec(self, rec_id):
         fetch_handle = Entrez.efetch(db=self.db, id=rec_id,
                                      rettype='Medline', retmode='json')
-        rec = fetch_handle.read()
+        rec = fetch_handle
         return rec
 
--- main.py
+++ main.py
@@ -1,37 +1,39 @@
-import sys
-from io import StringIO
 import pandas as pd
 import os.path
 from entrezService import EntrezSearchService
 from Bio import Medline
+from datetime import datetime
 
 
 def main(db, retstart, retmax, totalRecords, query, email, fileName):
+    print("Start time: ", datetime.now())
     entrez_service = EntrezSearchService(db)
-    rec_id_list = entrez_service.search_medline(retstart, retmax, totalRecords, query, email)
-    final_rec_list = {}
-    i = 0
-    for rec_id in rec_id_list:
-        rec = entrez_service.fetch_rec(rec_id)
-        rec_file = StringIO(rec.decode("utf-8"))
-        medline_rec = Medline.read(rec_file)
-        if 'AB' in medline_rec:
-            print(i, rec_id)
-            keys_values = medline_rec.items()
-            medline_rec = {str(key): str(value) for key, value in keys_values}
-            final_rec_list[i] = medline_rec
-            i = i + 1
+    rec_id_list = entrez_service.perform_esearch(retstart, retmax, totalRecords, query, email)
+    records = []
+    cleaned_records = []
+    missing_abstracts = 0
+    for search in rec_id_list:
+        rec = entrez_service.fetch_rec(search)
+        records.extend(list(Medline.parse(rec)))
+    for article in records:
+        if 'AB' in article:
+            keys_values = article.items()
+            casted_article = {str(key): str(value) for key, value in keys_values}
+            cleaned_records.append(casted_article)
         else:
-            print(i + " doesn't have abstract")
+            missing_abstracts += 1
 
-    print("Creating dataframe")
-    df = pd.DataFrame.from_dict(final_rec_list, orient='index')
+    print("Extraction end time: ", datetime.now())
+    print("missing abstracts: ", missing_abstracts)
+    print("Creating dataframe start: ", datetime.now())
+    df = pd.DataFrame(cleaned_records)
+    print("DF creation end time: ", datetime.now())
     my_path = os.path.abspath(os.path.dirname(__file__))
     print("Extracting to csv")
     path = os.path.join(my_path, "data\\" + fileName + ".csv")
     df.to_csv(path)
-
+    print("Extraction to csv time: ", datetime.now())
 
 if __name__ == '__main__':
-    # main("pmc", 0, 10, 10, "neuroscience", "your_email", "dataNLP")
-    globals()[sys.argv[1]](sys.argv[2])(sys.argv[3])(sys.argv[4])(sys.argv[5])(sys.argv[6])(sys.argv[7])
+    main("pubmed", 0, 10, 100, "neuroscience", "your_email", "dataNLP")
+    # globals()[sys.argv[1]](sys.argv[2])(sys.argv[3])(sys.argv[4])(sys.argv[5])(sys.argv[6])(sys.argv[7])
